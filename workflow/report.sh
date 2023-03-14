4606ecb7810627a11dfb2d8762eb9494c983ad11
Manabu ISHII <manabu.ishii.rb@gmail.com>
2023-03-14 12:06:24 +0900

    v005

--- workflow/report.sh
+++ workflow/report.sh
@@ -1,13 +1,13 @@
 # HTML
 mkdir -p report
-mkdir -p report/v004
-snakemake -s workflow/download.smk --report report/v004/download.html
-snakemake -s workflow/preprocess_known_human.smk --report report/v004/preprocess_known_human.html
-snakemake -s workflow/preprocess_known_otherspecies.smk --report report/v004/preprocess_known_otherspecies.html
-snakemake -s workflow/preprocess_putative_human.smk --report report/v004/preprocess_putative_human.html
-snakemake -s workflow/preprocess_putative_otherspecies.smk --report report/v004/preprocess_putative_otherspecies.html
-snakemake -s workflow/preprocess_putative_allspecies.smk --report report/v004/preprocess_putative_allspecies.html
-snakemake -s workflow/csv.smk --report report/v004/csv.html
-snakemake -s workflow/sqlite.smk --report report/v004/sqlite.html
-snakemake -s workflow/metadata.smk --report report/v004/metadata.html
-snakemake -s workflow/plot.smk --report report/v004/plot.html
+mkdir -p report/v005
+snakemake -s workflow/download.smk --report report/v005/download.html
+snakemake -s workflow/preprocess_known_human.smk --report report/v005/preprocess_known_human.html
+snakemake -s workflow/preprocess_known_otherspecies.smk --report report/v005/preprocess_known_otherspecies.html
+snakemake -s workflow/preprocess_putative_human.smk --report report/v005/preprocess_putative_human.html
+snakemake -s workflow/preprocess_putative_otherspecies.smk --report report/v005/preprocess_putative_otherspecies.html
+snakemake -s workflow/preprocess_putative_allspecies.smk --report report/v005/preprocess_putative_allspecies.html
+snakemake -s workflow/csv.smk --report report/v005/csv.html
+snakemake -s workflow/sqlite.smk --report report/v005/sqlite.html
+snakemake -s workflow/metadata.smk --report report/v005/metadata.html
+snakemake -s workflow/plot.smk --report report/v005/plot.html
